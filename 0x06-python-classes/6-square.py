b91174af50648679b1f06f607d908582bf5b18bf
tamara567 <ringatunu@gmail.com>
2023-10-24 11:09:18 -0700

    python classes

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -87,4 +87,4 @@ class Square:
             for i in range(self.__size):
                 for k in range(self.__position[0]):
                     print(" ",  end="")
-                print("#" * (self.__size
+                print("#" * (self.__size))
