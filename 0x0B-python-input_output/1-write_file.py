f1cfb6ef1d4dd926d181f2dcdac6c55c6ed77ebc
tamara567 <ringatunu@gmail.com>
2023-11-07 21:48:51 -0800

    input/output python

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""Module containing the function write_file"""
+""" Write to a file """
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF8) and returns the number
-    of characters written.
-
-    Args:
-        filename (str, optional): name of the file. Defaults to "".
-        text (str, optional): string of text to write to file. Defaults to "".
-
-    Returns:
-        int: number of characters written to file.
-    """
-    with open(filename, 'w', encoding="utf-8") as f:
-        """This method returns the number of characters written to a file."""
-        return f.write(text)
-
+    """ Writes a string to a text file (UTF8) and returns
+    the number of characters written """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return (f.write(text))
